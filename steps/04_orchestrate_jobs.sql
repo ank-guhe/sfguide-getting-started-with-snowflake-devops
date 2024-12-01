d5e36e221795ef3008fb20ecdf33596b1d455388
ank-guhe <ankush.guhe15@gmail.com>
2024-12-01 14:09:32 +0000

    New email id added

--- steps/04_orchestrate_jobs.sql
+++ steps/04_orchestrate_jobs.sql
@@ -80,7 +80,7 @@ create or alter task email_notification
       if (:options = '[]') then
         CALL SYSTEM$SEND_EMAIL(
             'email_integration',
-            'harnisha.paraye1994@gmail.com', -- INSERT YOUR EMAIL HERE
+            'ankush.guhe@gmail.com', -- INSERT YOUR EMAIL HERE
             'New data successfully processed: No suitable vacation spots found.',
             'The query did not return any results. Consider adjusting your filters.');
       end if;
@@ -93,14 +93,14 @@ create or alter task email_notification
 
       CALL SYSTEM$SEND_EMAIL(
         'email_integration',
-        'harnisha.paraye1994@gmail.com', -- INSERT YOUR EMAIL HERE
+        'ankush.guhe@gmail.com', -- INSERT YOUR EMAIL HERE
         'New data successfully processed: The perfect place for your summer vacation has been found.',
         :response);
     exception
         when EXPRESSION_ERROR then
             CALL SYSTEM$SEND_EMAIL(
             'email_integration',
-            'harnisha.paraye1994@gmail.com', -- INSERT YOUR EMAIL HERE
+            'ankush.guhe@gmail.com', -- INSERT YOUR EMAIL HERE
             'New data successfully processed: Cortex LLM function inaccessible.',
             'It appears that the Cortex LLM functions are not available in your region');
     end;
